69cb2b74953b6fa633379f4e52bd33fc169a183a
Rachel Rosalia <rachel@rosalia.me>
2017-11-04 17:50:09 -0700

    Support async/await

--- client/src/app.js
+++ client/src/app.js
@@ -1,16 +1,25 @@
+import 'babel-polyfill'
+
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-import styles from './styles.css';
+import styles from './styles.css'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { text: 'Loading...' }
+  }
+
+  async componentDidMount() {
+    const response = await fetch('api')
+    const message = await response.text()
+    this.setState({ text: message })
+  }
+
   render() {
-    return (
-      <div className={styles.hello}>
-        Hello world!
-      </div>
-    )
+    return <div className={styles.hello}>{this.state.text}</div>
   }
 }
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById('app'))
